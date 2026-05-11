65102a0fe694ffc8045eba4050d4f514d50376b4
ARLPYP <arlen.pyper@student.adelaide.edu.au>
2026-05-11 15:48:11 +0930

    My commit :3
    hello guys whats good :)

---
+++ BoardGame.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <vector>
+using namespace std;
+class Person;
+
+class BoardGame
+{
+public:
+	vector<Person> designers;
+	string genre;
+	int reccomendedPlayers;
+private:
+};
