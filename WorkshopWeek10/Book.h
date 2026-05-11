e0d56f2e7c2398fc340e818bc20db911c525289e
kevinkipro <kipky008@mymail.unisa.edu.au>
2026-05-11 15:52:43 +0930

    add Book, Catalouge, Hold, Magazine, Toy Headers

---
+++ WorkshopWeek10/Book.h
@@ -0,0 +1,22 @@
+#pragma once
+#pragma once
+
+#pragma once
+
+#pragma once
+
+#include <vector>
+#include <string>
+
+class Person;
+using namespace std;
+
+class Book
+{
+private:
+    string genre;
+    Person* author;
+
+public:
+    Book();
+};
